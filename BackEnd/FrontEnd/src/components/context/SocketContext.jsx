afcd51985c55d80eb23e737e515be4f6d28f061e
vivek6201 <gvicky45678@gmail.com>
2024-05-05 00:15:22 +0530

    url updated

--- BackEnd/FrontEnd/src/components/context/SocketContext.jsx
+++ BackEnd/FrontEnd/src/components/context/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:4001", {
+      const socket = io("https://chatapp-yt.onrender.com", {
         query: {
           userId: authUser._id,
         },
